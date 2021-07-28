366088a85acdb2fd243924c5d2d5d740298a82f2
Rahmat Agung Julians <rahmatagungj@gmail.com>
2021-07-28 19:32:33 +0700

    Adding frame into information panel

--- frontend/src/pages/InformationAdmin.js
+++ frontend/src/pages/InformationAdmin.js
@@ -4,7 +4,13 @@ import DashboardAdmin from "../layouts/DashboardAdmin";
 function InformationAdmin(props) {
     return (
         <DashboardAdmin>
-            hai admin
+            <div className="container border-t-4 border-primary bg-white p-5 rounded">
+                <h3 className="text-base font-bold text-primary">Informasi Pemilihan (Admin)</h3>
+                <hr className="my-5"/>
+                <div className="mx-auto container flex items-center justify-evenly w-full">
+                    konten taroh sini cok
+                </div>
+            </div>
         </DashboardAdmin>
     );
 }
--- frontend/src/pages/InformationPublic.js
+++ frontend/src/pages/InformationPublic.js
@@ -4,40 +4,11 @@ import DashboardPublic from "../layouts/DashboardPublic";
 function InformationPublic() {
     return (
         <DashboardPublic>
-            <div tabIndex="0" className="collapse w-96 rounded-box border border-base-300 collapse-arrow">
-                <div className="collapse-title text-xl font-medium">
-                    I open with focus
-                </div>
-                <div className="collapse-content">
-                    <p>Collapse content reveals with focus. If you add a checkbox, you can control it using checkbox
-                        instead of focus. Or you can force-open/force-close using
-                        <span className="badge badge-outline">collapse-open</span> and
-                        <span className="badge badge-outline">collapse-close</span> classes.
-                    </p>
-                </div>
-            </div>
-            <div tabIndex="0" className="collapse w-96 rounded-box border border-base-300 collapse-arrow">
-                <div className="collapse-title text-xl font-medium">
-                    I open with focus
-                </div>
-                <div className="collapse-content">
-                    <p>Collapse content reveals with focus. If you add a checkbox, you can control it using checkbox
-                        instead of focus. Or you can force-open/force-close using
-                        <span className="badge badge-outline">collapse-open</span> and
-                        <span className="badge badge-outline">collapse-close</span> classes.
-                    </p>
-                </div>
-            </div>
-            <div tabIndex="0" className="collapse w-96 rounded-box border border-base-300 collapse-arrow">
-                <div className="collapse-title text-xl font-medium">
-                    I open with focus
-                </div>
-                <div className="collapse-content">
-                    <p>Collapse content reveals with focus. If you add a checkbox, you can control it using checkbox
-                        instead of focus. Or you can force-open/force-close using
-                        <span className="badge badge-outline">collapse-open</span> and
-                        <span className="badge badge-outline">collapse-close</span> classes.
-                    </p>
+            <div className="container border-t-4 border-primary bg-white p-5 rounded">
+                <h3 className="text-base font-bold text-primary">Informasi Pemilihan</h3>
+                <hr className="my-5"/>
+                <div className="mx-auto container flex items-center justify-evenly w-full">
+                    konten taroh sini cok
                 </div>
             </div>
         </DashboardPublic>
