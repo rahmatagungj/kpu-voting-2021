8525eab5d35e328e57dd6bf18bb174c27128b9f0
Rahmat Agung Julians <rahmatagungj@gmail.com>
2021-07-28 19:26:17 +0700

    Small patch for header name

--- frontend/src/layouts/DashboardAdmin.js
+++ frontend/src/layouts/DashboardAdmin.js
@@ -18,16 +18,22 @@ function DashboardAdmin(props) {
       <div className="shadow-lg bg-base-200 drawer drawer-mobile h-screen">
         <input id="my-drawer-2" type="checkbox" className="drawer-toggle" />
         <div className="flex flex-col drawer-content">
-          <header className="bg-primary w-full h-13 z-40 flex items-center justify-between sticky top-0 z-50 shadow-sm sm:shadow-none p-2">
-            <div className="block lg:hidden ml-6">
-              <label htmlFor="my-drawer-2" className="cursor-pointer flex p-2 items-center rounded-full text-gray-400 hover:text-gray-700 bg-white shadow text-md drawer-button lg:hidden">
-                <svg width="20" height="20" className="text-gray-400 hover:text-gray-700" fill="currentColor" viewBox="0 0 1792 1792" xmlns="http://www.w3.org/2000/svg">
-                  <path d="M1664 1344v128q0 26-19 45t-45 19h-1408q-26 0-45-19t-19-45v-128q0-26 19-45t45-19h1408q26 0 45 19t19 45zm0-512v128q0 26-19 45t-45 19h-1408q-26 0-45-19t-19-45v-128q0-26 19-45t45-19h1408q26 0 45 19t19 45zm0-512v128q0 26-19 45t-45 19h-1408q-26 0-45-19t-19-45v-128q0-26 19-45t45-19h1408q26 0 45 19t19 45z"></path>
+          <header
+              className="bg-primary w-full h-13 z-40 flex items-center justify-between sticky top-0 z-50 shadow-sm sm:shadow-none p-2">
+            <div className="block lg:hidden ml-3">
+              <label htmlFor="my-drawer-2"
+                     className="cursor-pointer flex p-2 items-center rounded-full text-gray-400 hover:text-gray-700 shadow text-md drawer-button lg:hidden">
+                <svg width="22" height="22" className="text-gray-400 hover:text-gray-700"
+                     fill="#ffffff"
+                     viewBox="0 0 1792 1792" xmlns="http://www.w3.org/2000/svg">
+                  <path
+                      d="M1664 1344v128q0 26-19 45t-45 19h-1408q-26 0-45-19t-19-45v-128q0-26 19-45t45-19h1408q26 0 45 19t19 45zm0-512v128q0 26-19 45t-45 19h-1408q-26 0-45-19t-19-45v-128q0-26 19-45t45-19h1408q26 0 45 19t19 45zm0-512v128q0 26-19 45t-45 19h-1408q-26 0-45-19t-19-45v-128q0-26 19-45t45-19h1408q26 0 45 19t19 45z">
+                  </path>
                 </svg>
               </label>
             </div>
             <div className="relative z-20 flex flex-col justify-end h-full px-3 md:w-full">
-              <div className="relative p-1 flex items-center w-full space-x-4 justify-end">
+              <div className="relative p-2 flex items-center w-full space-x-4 justify-end">
                 <div className="dropdown dropdown-end">
                   <button className="flex items-center text-white dark:text-white text-md" tabIndex={0}>
                     Rahmat AJ
