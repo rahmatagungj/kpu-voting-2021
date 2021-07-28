52cebcb9c855d9f4eac2feb539d332fedfdb4e5a
Rahmat Agung Julians <rahmatagungj@gmail.com>
2021-07-28 18:05:35 +0700

    Refactor for login css

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -14,7 +14,7 @@ function Login(props) {
     }
 
     return (
-        <div className="relative min-h-screen flex">
+        <div className="relative min-h-screen flex withBackground">
             <div
                 className="flex sm:flex-row items-center md:items-start sm:justify-center md:justify-start flex-auto min-w-0 md:bg-white bg-primary bg-opacity-25">
                 <div
