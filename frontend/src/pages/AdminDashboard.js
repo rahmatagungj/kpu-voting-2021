da332fc892e2a768823091fe038e085652a5ab80
Rahmat Agung Julians <rahmatagungj@gmail.com>
2021-07-29 15:49:42 +0700

    Add new modules

--- frontend/src/pages/AdminDashboard.js
+++ frontend/src/pages/AdminDashboard.js
@@ -1,31 +1,59 @@
-import React from 'react';
-import {dataStat} from "../data/dataStat";
+import React, {useEffect, useState} from 'react';
 import DashboardAdmin from "../layouts/DashboardAdmin";
 import Seo from "../components/Seo"
+import axios from "axios"
 
 function AdminDashboard() {
+    const [countVoteOne, setCountVoteOne] = useState(0)
+    const [countVoteTwo, setCountVoteTwo] = useState(0)
+    const [isLoading, setIsloading] = useState(false)
+
+    useEffect(() => {
+        setIsloading(true)
+        axios.get('https://kpu-stkip.azurewebsites.net/api/vote/count/1')
+            .then(response => setCountVoteOne(response.data.message))
+            .catch((e) => setCountVoteOne("Error"));
+        axios.get('https://kpu-stkip.azurewebsites.net/api/vote/count/2')
+            .then(response => setCountVoteTwo(response.data.message))
+            .catch((e) => setCountVoteTwo("Error"));
+        setIsloading(false)
+
+    }, [])
 
     const RenderStat = () => {
         return (
             <div className="flex justify-center mb-5 w-full">
                 <div className="stats shadow w-full rounded">
-                    {
-                        dataStat.map(data => (
-                            <div className="stat" key={data.key}>
-                                <div className="stat-figure text-info">
-                                    <div className="avatar">
-                                        <div className="w-16 h-16 p-1 mask mask-squircle bg-base-100">
-                                            <img src={data.image}
-                                                 alt="Avatar Tailwind CSS Component" className="mask mask-squircle"/>
-                                        </div>
-                                    </div>
+                    <div className="stat">
+                        <div className="stat-figure text-info">
+                            <div className="avatar">
+                                <div className="w-16 h-16 p-1 mask mask-squircle bg-base-100">
+                                    <img src="https://i.ibb.co/GTwgFwH/IMG-5492-removebg-preview.png"
+                                         alt="Avatar Tailwind CSS Component" className="mask mask-squircle"/>
                                 </div>
-                                <div className="stat-value">{data.voted}%</div>
-                                <div className="stat-title">{data.name}</div>
                             </div>
-                        ))
-                    }
-
+                        </div>
+                        <div
+                            className="stat-value">{isLoading ? "Memuat " :countVoteOne} %
+                        </div>
+                        <div className="stat-title">{isLoading ? "Memuat " : countVoteOne} Suara</div>
+                        <div className="stat-title">Handika Rahmat Utama & Muhammad Abdul Aziz</div>
+                    </div>
+                    <div className="stat">
+                        <div className="stat-figure text-info">
+                            <div className="avatar">
+                                <div className="w-16 h-16 p-1 mask mask-squircle bg-base-100">
+                                    <img src="https://i.ibb.co/yY2PPgw/IMG-2923-removebg-preview.png"
+                                         alt="Avatar Tailwind CSS Component" className="mask mask-squircle"/>
+                                </div>
+                            </div>
+                        </div>
+                        <div
+                            className="stat-value">{isLoading ? "Memuat " : countVoteTwo} %
+                        </div>
+                        <div className="stat-title">{isLoading ? "Memuat " : countVoteTwo} Suara</div>
+                        <div className="stat-title">Pitradi & Robi Iskandar</div>
+                    </div>
                 </div>
             </div>
         )
