39d094151f453a608f0163c3de86e2fe4d742a83
Rahmat Agung Julians <rahmatagungj@gmail.com>
2021-08-03 18:45:31 +0700

    add limit fixed number of count vote

--- frontend/src/pages/AdminDashboard.js
+++ frontend/src/pages/AdminDashboard.js
@@ -10,7 +10,7 @@ function getPercentage(countVote, totalVote) {
   const result = (countVote / totalVote) * 100;
 
   if (!isNaN(result)) {
-    return result;
+    return result.toFixed(1);
   } else {
     return 0;
   }
